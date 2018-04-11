37ea32b5fed89669cba2c0cafc1652e17bacd2e7
Takuya Kajiyama <Kajitaku@users.noreply.github.com>
2018-04-11 15:14:11 +0900

    p159

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -5,16 +5,9 @@ from WasRun import WasRun
 
 class TestCaseTest(TestCase):
 
-    def setUp(self):
-        self.test = WasRun("testMethod")
+    def testTemplateMethod(self):
+        test = WasRun('testMethod')
+        test.run()
+        assert('setUp testMethod ' == test.log)
 
-    def testRunning(self):
-        self.test.run()
-        assert('setUp testMethod ' == self.test.log)
-
-    def testSetUp(self):
-        self.test.run()
-        assert('setUp testMethod ' == self.test.log)
-
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
